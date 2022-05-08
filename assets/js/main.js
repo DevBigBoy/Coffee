be7e7100d031e9a7c9339d9993d285041332f249
DevMohamedElshazly <moshazly416@gmail.com>
2022-05-08 21:51:40 +0200

    project-finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,26 @@
 /*=============== LOADER ===============*/
+onload = () => {
+  const load = document.getElementById("load");
+
+  setTimeout(() => {
+    load.style.display = "none";
+  }, 2500);
+};
+/*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById("nav-menu"),
   navClose = document.getElementById("nav-close"),
   navToggle = document.getElementById("nav-toggle");
 
-/*=============== SHOW MENU ===============*/
-// Validate if constant exists
-
+// TODO: MENU SHOW
+//* Validate if constant exists
 if (navToggle) {
   navToggle.addEventListener("click", () => {
     navMenu.classList.add("show-menu");
   });
 }
 
+// TODO: MENU HIDDEN
+//* Validate if constant exists
 if (navClose) {
   navClose.addEventListener("click", () => {
     navMenu.classList.remove("show-menu");
